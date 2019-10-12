e887f665d70d039b122e3d931f97083f7398762f
Zhangzhe <yjzheng2017@163.com>
2019-10-12 23:11:26 +0800

    Add inline declare.

--- QueryResult.h
+++ QueryResult.h
@@ -17,7 +17,7 @@ class QueryResult {
 	 std::shared_ptr<std::vector<std::string>> file;
 };
 
-std::string make_plural(size_t ctr, const std::string& word, const std::string& ending){
+inline std::string make_plural(size_t ctr, const std::string& word, const std::string& ending){
 	return (ctr == 1) ? word : word + ending;
 }
 
